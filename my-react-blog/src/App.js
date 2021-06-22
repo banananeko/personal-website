205f7fb09b2ac33edd1853d6e7217d9bdd6f7b4e
bananneko <yli497@aucklanduni.ac.nz>
2021-06-22 23:25:22 +1200

    Finished hamburger icon

--- my-react-blog/src/App.js
+++ my-react-blog/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React  from 'react';
 import Navigation from './components/navigation'
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
-
 import PageRenderer from './page-renderer'
 function App() {
   return (
--- my-react-blog/src/components/navigation.js
+++ my-react-blog/src/components/navigation.js
@@ -1,6 +1,7 @@
-import React,  {useState} from 'react'
+import React, { useState, useRef }  from 'react'
 import {Link} from 'react-router-dom'
 import {Avatar} from 'antd'
+import { useOnClickOutside } from './hook';
 
 const navLinks = [
     {
@@ -28,15 +29,19 @@ const navLinks = [
 
 export default function Navigation () {
     const [menuActive, setMenuActive] = useState(false)
+    const node = useRef(); 
+    useOnClickOutside(node, () => setMenuActive(false));
+
     return (
     <nav className="site-navigation" role="navigation">
         
         <span className="menu-title">
             <span>
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38}/><Link to='/'>Yiwei Li</Link>
+                {/* <img src="../assets/images/橘猫头像Banananeko.jpg" size={38}/> */}
+                <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7VokmrwrMO7pnA3j1R0llY1PWdD9Y9hAO6A&usqp=CAU" size={38} /><Link to='/'>Yiwei Li</Link>
             </span>
         </span>
-        <div className={`menu-content-container ${menuActive && 'active'}`}>
+        <div ref={node} className={`menu-content-container ${menuActive && 'active'}`}>
             <ul>
                 { navLinks.map((link, index) => (
                     <li key={index}>
@@ -45,6 +50,13 @@ export default function Navigation () {
                 ))}
             </ul>
         </div>
-        <i className="icon ionicons ion-ios-menu" onClick={(ev) => setMenuActive(!menuActive)} />
+
+        <div className={`iconWrapper ${menuActive && 'active'}`}>
+          <div className="hamburger " onClick={(ev) => setMenuActive(!menuActive)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
     </nav>)
 }
